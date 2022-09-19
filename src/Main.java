40473152736220d43c76374fecd81c5f99a36016
khaledzaza <zazak886@gmail.com>
2022-09-19 15:49:16 -0700

    Ask for name

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello " );
+        System.out.println("What is your name? " );
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+
+
+        System.out.println("Hello "+ name);
+
 
 
     }
